6ad025287a6742dd0f7285873f28c81f7db130a3
AnandGupta <anand.gupta@yantriks.com>
2018-07-19 12:02:21 -0400

    Springboot JWT App

---
+++ src/main/java/com/nouhoun/springboot/jwt/integration/controller/ResourceController.java
@@ -0,0 +1,52 @@
+package com.nouhoun.springboot.jwt.integration.controller;
+
+import com.nouhoun.springboot.jwt.integration.domain.RandomCity;
+import com.nouhoun.springboot.jwt.integration.domain.User;
+import com.nouhoun.springboot.jwt.integration.service.GenericService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by nydiarra on 06/05/17.
+ */
+@RestController
+//@RequestMapping("/springjwt")
+public class ResourceController {
+    @Autowired
+    private GenericService userService;
+
+    @RequestMapping(value ="/cities")
+    @PreAuthorize("hasAuthority('ADMIN_USER') or hasAuthority('STANDARD_USER')")
+    public List<RandomCity> getUser(){
+        return userService.findAllRandomCities();
+    }
+
+    @RequestMapping(value ="/users", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN_USER')")
+    public List<User> getUsers(){
+        return userService.findAllUsers();
+    }
+    
+    @RequestMapping(value ="/parse", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('ADMIN_USER')")
+    @ResponseBody
+    @ResponseStatus(code=HttpStatus.OK,value=HttpStatus.OK)
+    public String parse(@RequestBody(required = false) String parsedDataList) {
+      
+      System.out.println("REQUEST_BODY --> " + parsedDataList);
+      
+      return "SUCCESS"+System.currentTimeMillis();
+  }
+    
+    
+}
